a4daabdd273cc9de6feedd17c83ae39a1c4cced2
abattagl <abattagl@ROVERF.fritz.box>
2020-08-14 17:43:27 +0200

    added multi build script

---
+++ buildQiot.sh
@@ -0,0 +1,17 @@
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/qiot-datahub-storer-gas/
+./build.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/qiot-datahub-storer-pollution/
+./build.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-aggregation/
+./build.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-importer/
+./build.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-registration/
+./build.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-endpoint/qiot-datahub-endpoint-mqtt/
+./build.sh
+cd /home/abattagl/git/qiot/edge/qiot-sensors-emulator/
+./build.sh
+cd /home/abattagl/git/qiot/edge/qiot-service/
+./build.sh
+
