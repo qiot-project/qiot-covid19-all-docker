2dc17afcfe3797a58fdad3d0c5959fc6c58a44a1
abattagl <abattagl@192.168.0.101>
2020-08-25 07:54:42 +0200

    added "test" prefix to station serial and name for testing

--- comp/init-mongo.js
+++ comp/init-mongo.js
@@ -1,12 +1,14 @@
+use qiot
 db.createUser(
-  {
-	user : "qiot",
-	pwd : "qiot",
-	roles : [
-	  {
-	    role : "readWrite",
-	    db : "qiot"
-	  }
-	]
-  }
+   {
+     user: "qiot",
+     pwd: "qiot",
+     roles: [ { role: "readWrite", db: "qiot" } ]
+   }
+)
+db.counters.insert(
+ {
+      _id: "userid",
+      seq: NumberInt(0)
+   }
 )
