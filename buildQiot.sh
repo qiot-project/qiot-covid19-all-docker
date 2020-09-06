10ccce17698f4e9a9b4827d512d26a268902c249
abattagl <abattagl@ROVERF.fritz.box>
2020-09-06 04:22:07 +0200

    created sensors file added env prop for custom %staging profile when running on local docker instance fixes

--- buildQiot.sh
+++ buildQiot.sh
@@ -4,14 +4,14 @@ cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/qiot-datahub-storer-pollu
 ./build.sh
 cd /home/abattagl/git/qiot/datahub/qiot-datahub-aggregation/
 ./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-importer/
-./build.sh
+#cd /home/abattagl/git/qiot/datahub/qiot-datahub-importer/
+#./build.sh
 cd /home/abattagl/git/qiot/datahub/qiot-datahub-registration/
 ./build.sh
 cd /home/abattagl/git/qiot/datahub/qiot-datahub-endpoint/qiot-datahub-endpoint-mqtt/
 ./build.sh
-cd /home/abattagl/git/qiot/edge/qiot-sensors-emulator/
-./build.sh
-cd /home/abattagl/git/qiot/edge/qiot-service/
-./build.sh
+#cd /home/abattagl/git/qiot/edge/qiot-sensors-emulator/
+#./build.sh
+#cd /home/abattagl/git/qiot/edge/qiot-service/
+#./build.sh
 
