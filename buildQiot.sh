aa2d9d79504420e23ef42dff240c168822fc973b
abattagl <abattagl@localhost>
2021-02-12 09:12:43 +0100

    Commit for tested beta verion

--- buildQiot.sh
+++ buildQiot.sh
@@ -1,24 +1,23 @@
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/qiot-datahub-storer-gas/
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-localization/
 chmod +x build.sh
 ./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/qiot-datahub-storer-pollution/
-chmod +x build.sh
-./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-aggregation/
-chmod +x build.sh
-./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-query/
-chmod +x build.sh
-./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-importer/
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-station/
 chmod +x build.sh
 ./build.sh
 cd /home/abattagl/git/qiot/datahub/qiot-datahub-registration/
 chmod +x build.sh
 ./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-endpoint/qiot-datahub-endpoint-mqtt/
-chmod +x build.sh
-./build.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-collector/
+chmod +x build*.sh
+./buildgas.sh
+./buildpollution.sh
+cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/
+chmod +x build*.sh
+./buildgas.jvm.sh
+./buildpollution.jvm.sh
+#cd /home/abattagl/git/qiot/datahub/qiot-datahub-importer/
+#chmod +x build.sh
+#./build.sh
 #cd /home/abattagl/git/qiot/edge/qiot-sensors-emulator/
 #chmod +x build.sh
 #./build.sh
