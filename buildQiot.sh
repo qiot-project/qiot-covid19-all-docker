79d3836a2e0c58f1912bd2ad95107336a2d0c404
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-03-02 10:45:31 +0100

    switched to postgresql

--- buildQiot.sh
+++ buildQiot.sh
@@ -1,27 +1,27 @@
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-localization/
+cd ~/git/qiot/datahub/qiot-datahub-localization/
 chmod +x build.sh
 ./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-station/
+cd ~/git/qiot/datahub/qiot-datahub-station/
 chmod +x build.sh
 ./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-registration/
+cd ~/git/qiot/datahub/qiot-datahub-registration/
 chmod +x build.sh
 ./build.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-collector/
+cd ~/git/qiot/datahub/qiot-datahub-collector/
 chmod +x build*.sh
 ./buildgas.sh
 ./buildpollution.sh
-cd /home/abattagl/git/qiot/datahub/qiot-datahub-storer/
+cd ~/git/qiot/datahub/qiot-datahub-storer/
 chmod +x build*.sh
 ./buildgas.jvm.sh
 ./buildpollution.jvm.sh
-#cd /home/abattagl/git/qiot/datahub/qiot-datahub-importer/
+cd ~/git/qiot/datahub/qiot-datahub-importer/
+chmod +x build.jvm.sh
+./build.jvm.sh
+#cd ~/git/qiot/edge/qiot-sensors-emulator/
 #chmod +x build.sh
 #./build.sh
-#cd /home/abattagl/git/qiot/edge/qiot-sensors-emulator/
-#chmod +x build.sh
-#./build.sh
-#cd /home/abattagl/git/qiot/edge/qiot-service/
+#cd ~/git/qiot/edge/qiot-service/
 #chmod +x build.sh
 #./build.sh
 
